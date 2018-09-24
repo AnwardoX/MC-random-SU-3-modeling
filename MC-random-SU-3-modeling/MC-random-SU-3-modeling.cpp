371dd5e2f9fb1fbf9faa52e3ce7c53170746ce0d
Anwardo <anton30061997@gmail.com>
2018-09-24 22:53:04 +0300

    Changes: - adjusetd the limit to be not a power of 2 - Coded a dist_2018 generator, that generates in (-limit; limit) - commented out all "limit" versions of dsitributions - too slow - wrote tests for pait <your generator here> + dist_2018

--- MC-random-SU-3-modeling/MC-random-SU-3-modeling.cpp
+++ MC-random-SU-3-modeling/MC-random-SU-3-modeling.cpp
@@ -16,7 +16,7 @@
 
 static const int16_t n = 8;
 // should be less than 64-bit type due to dist_2018
-static const int16_t limit = 2048;
+static const int16_t limit = 1743;
 
 using namespace std;
 using namespace chrono;
@@ -43,37 +43,46 @@ public:
 	}
 };
 
+//generates numbers from (-limit;limit)
 class dist_2018
 {
 public:
-	typedef int16_t result_type;
-	dist_2018(const result_type _s) :
-		s(_s),
-		t((gen_max_power_2 - s) % s) {}
+	typedef int32_t result_type;
+	dist_2018(const result_type _limit) :
+		limit(_limit),
+		s(2 * _limit - 1),
+		t( (static_cast<uint64_t>(~(s - 1))) % s ) //in this line (2^64 - s) is calculated as (~(s - 1))
+	{}
 
-	result_type operator()(const function<uint64_t()> &gen) const {
-		uint64_t x  = gen();
-		         x &= gen_max;
-		int64_t m = x * s;
-		int64_t l = m % gen_max_power_2;
-		if (l < s) {
-			while (l < t) {
-				x = gen();
-				m = x * s;
-				l = m % gen_max_power_2;
-			}
+	result_type operator()(const function<uint64_t()> &gen) const
+	{
+		uint64_t x, m;
+		uint32_t l = 0;
+		while (l < t) 
+		{
+			x = gen() & gen_output_mask; //get first 32 bits
+			m = x * s;
+			l = m >> gen_output_number_of_bits; //get a remainder of division by 2^32
 		}
-		return m / gen_max_power_2;
+		int32_t almost_output = m >> output_number_of_bits; //get a quotient from division by 2^32
+		return almost_output - limit;
 	}
 
 private:
 	// upper bound
 	result_type s;
-	const int64_t t;
+	const uint32_t t;
+	const int64_t limit;
 	// assuming 64-bit generator
-	static constexpr uint64_t gen_max = numeric_limits<uint64_t>::max() >> (sizeof(limit) * 8);
+	//mask to extract first 32 bits from generator output
+	static constexpr uint64_t gen_output_mask = numeric_limits<uint32_t>::max();
+	//number of bits to shift for division by 2^32
+	static constexpr uint8_t gen_output_number_of_bits = 8 * sizeof(uint64_t);
+	//number of bits in output for division  for division by 2^L
+	static constexpr uint8_t output_number_of_bits = 8 * sizeof(result_type);
+	//static constexpr uint64_t gen_max = numeric_limits<uint64_t>::max() >> (sizeof(limit) * 8);
 	// overflow if limit's type width is 64 bits
-	static constexpr uint64_t gen_max_power_2 = gen_max + 1; 
+	//static constexpr uint64_t gen_max_power_2 = gen_max + 1; 
 };
 
 tuple<int16_t, int16_t, int16_t, int16_t> generate_ref() {
@@ -151,11 +160,14 @@ int main() {
 	}*/
 
 	static const int64_t test_count = 20;
-	static const int64_t repeat_count = 100000000;
+	static const int64_t repeat_count = 10;
 
 	pcg_extras::seed_seq_from<random_device> seed_source;
 	uniform_int_distribution<int64_t> dist(-limit, limit);
+	dist_2018 dist_2k18(limit);
 
+	/*
+	//no need in this, as it is not wirkong with arbitrary limits
 	cout << "reference (4 x 16 bit, incorrect)" << endl;
 	tuple<int16_t, int16_t, int16_t, int16_t> t1;
 	long long time_total = 0;
@@ -170,10 +182,11 @@ int main() {
 	}
 	cout << "Average time: " << double(time_total) / test_count << " ms" << endl;
 	cout << endl;
+	*/
 
-	cout << "reference (truncate, correct)" << endl;
+	cout << "reference (real2, approx. correct)" << endl;
 	int16_t t2;
-	time_total = 0;
+	long long time_total = 0;
 	for (int j = 0; j < test_count; j++) {
 		auto start = system_clock::now();
 		for (int i = 0; i < repeat_count; i++)
@@ -202,6 +215,8 @@ int main() {
 	cout << "Average time: " << double(time_total) / test_count << " ms" << endl;
 	cout << endl;
 
+	/*
+	//veeeery slow guy
 	cout << "pcg64 + limit" << endl;
 	pcg64_oneseq_once_insecure pcg64_2(seed_source);
 	time_total = 0;
@@ -221,7 +236,10 @@ int main() {
 	}
 	cout << "Average time: " << double(time_total) / test_count << " ms" << endl;
 	cout << endl;
+	*/
 
+	/*
+	//very slow guy
 	cout << "genrand int + limit" << endl;
 	time_total = 0;
 	for (int j = 0; j < test_count; j++) {
@@ -240,7 +258,10 @@ int main() {
 	}
 	cout << "Average time: " << double(time_total) / test_count << " ms" << endl;
 	cout << endl;
+	*/
 
+	/*
+	//not working for some reason
 	cout << "genrand + stl distribution" << endl;
 	genrand g;
 	int16_t t4;
@@ -256,6 +277,7 @@ int main() {
 	}
 	cout << "Average time: " << double(time_total) / test_count << " ms" << endl;
 	cout << endl;
+	*/
 
 	cout << "stl mt + stl distribution" << endl;
 	mt19937_64 mt;
@@ -265,6 +287,57 @@ int main() {
 		auto start = system_clock::now();
 		for (int i = 0; i < repeat_count; i++)
 			t5 = dist(mt);
+		auto end = system_clock::now();
+		auto elapsed = duration_cast<milliseconds>(end - start).count();
+		time_total += elapsed;
+		//cout << "Time elapsed: " << elapsed << " ms" << endl;
+	}
+	cout << "Average time: " << double(time_total) / test_count << " ms" << endl;
+	cout << endl;
+
+	cout << "stl mt + dist_2018" << endl;
+	mt19937_64 mt2;
+	int16_t t6;
+	time_total = 0;
+	for (int j = 0; j < test_count; j++) {
+		auto start = system_clock::now();
+		for (int i = 0; i < repeat_count; i++)
+			t6 = dist_2k18(mt2);
+		auto end = system_clock::now();
+		auto elapsed = duration_cast<milliseconds>(end - start).count();
+		time_total += elapsed;
+		//cout << "Time elapsed: " << elapsed << " ms" << endl;
+	}
+	cout << "Average time: " << double(time_total) / test_count << " ms" << endl;
+	cout << endl;
+
+	/*
+	//not working either due to genrand
+	cout << "genrand + dist_2018" << endl;
+	genrand g2;
+	int16_t t7;
+	time_total = 0;
+	for (int j = 0; j < test_count; j++) {
+		auto start = system_clock::now();
+		for (int i = 0; i < repeat_count; i++)
+			t7 = dist_2k18(g2);
+		auto end = system_clock::now();
+		auto elapsed = duration_cast<milliseconds>(end - start).count();
+		time_total += elapsed;
+		//cout << "Time elapsed: " << elapsed << " ms" << endl;
+	}
+	cout << "Average time: " << double(time_total) / test_count << " ms" << endl;
+	cout << endl;
+	*/
+
+	cout << "pcg64 + dist_2018" << endl;
+	pcg64_oneseq_once_insecure pcg64_2(seed_source);
+	int16_t t8;
+	time_total = 0;
+	for (int j = 0; j < test_count; j++) {
+		auto start = system_clock::now();
+		for (int i = 0; i < repeat_count; i++)
+			t8 = dist_2k18(pcg64_2);
 		auto end = system_clock::now();
 		auto elapsed = duration_cast<milliseconds>(end - start).count();
 		time_total += elapsed;
